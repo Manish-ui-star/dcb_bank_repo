83be0919470e8e61326040da6be6934b13ab908b
Manish-ui-star <manishjaiswal10590@gmail.com>
2022-11-12 08:43:36 +0530

    again

--- demo.java
+++ demo.java
@@ -7,7 +7,15 @@ public static void main(String [] args )
    System.out.println("Welcome to git bash":);
 }
 
+<<<<<<< Updated upstream
 public void m2() {
   ///
+=======
+public void m1()
+{
+
+  //
+   }
+>>>>>>> Stashed changes
 
 }
